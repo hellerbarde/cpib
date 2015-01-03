72e95f9b7f7c4a39e90cae53f385b8140f1b2e4f
Philip Stark <git@codechaos.ch>
2015-01-03 22:36:21 +0100

    remove debugging output from Compiler.cs

--- Compiler/Compiler/Compiler.cs
+++ Compiler/Compiler/Compiler.cs
@@ -9,15 +9,15 @@ namespace Compiler
     static void Main(string[] args)
     {
       try {
-        Console.WriteLine("args: " + args);
+        Console.WriteLine("args: (" + String.Join(",",args)+")");
         var scanner = new Scanner();
         var list = scanner.Scan(new System.IO.StreamReader("test02.iml"));
-        Console.WriteLine("[" + String.Join(",\n", list) + "]");
+        //Console.WriteLine("[" + String.Join(",\n", list) + "]");
         var parser = new Parser(list);
         IProgram cst = parser.parseprogram();
         //Console.WriteLine(program);
         var ast = cst.ToAbstractSyntax();
-        Console.WriteLine(ast.ToString());
+        //Console.WriteLine(ast.ToString());
         if(!(ast is ASTProgram))
         {
           throw new IVirtualMachine.InternalError("Generation of Abstract Syntax Tree failed.");
@@ -26,7 +26,7 @@ namespace Compiler
 
         var stringbuilder = new StringBuilder();
         program.printAST(0, stringbuilder);
-        Console.Write(stringbuilder.ToString());
+        //Console.Write(stringbuilder.ToString());
         //return;
         //Checker
         CheckerInformation info = new CheckerInformation();
@@ -35,8 +35,8 @@ namespace Compiler
         //Code Generator
         IVirtualMachine vm = new VirtualMachine(1000, 1000);
         program.GenerateCode(0, vm, info);
-        Console.WriteLine(vm.ToString());
-        Console.WriteLine();
+        //Console.WriteLine(vm.ToString());
+        //Console.WriteLine();
         //Execution
         vm.Execute();
       } catch (Exception ex) {
